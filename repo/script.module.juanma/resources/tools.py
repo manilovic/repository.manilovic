e9ffdceaf4a612f4908cb3f181539870b685b3d0
Juanma <juanma_fr@hotmail.com>
2024-11-29 20:20:52 +0100

    6.31

--- repo/script.module.juanma/resources/tools.py
+++ repo/script.module.juanma/resources/tools.py
@@ -88,30 +88,7 @@ def notificacion(line1):
     timeml = 5000 #in miliseconds
     xbmc.executebuiltin('Notification(%s, %s, %d, %s)'%(__addonname__,line1, timeml, __icon__))
     
-    
-    
-def replace_opcion(name):
-     
-    name = name.replace("c1", "1") 
-    name = name.replace("c2", "2")
-    name = name.replace("o1", "Alternativo 1")
-    name = name.replace("o2", "Alternativo 2")
-    name = name.replace("o3", "Alternativo 3")
-    name = name.replace("o-3", "Alternativo 3")
-    name = name.replace("o4", "Alternativo 4")
-    name = name.replace("o5", "Alternativo 5")
-    name = name.replace("02", "Alternativo 2")
-    name = name.replace("03", "Alternativo 3")
-    name = name.replace("m-ligatv", "LaLiga TV-")
-    name = name.replace("channel","")  
-    name = name.replace("deportes","M. Deportes-") 
-    name = name.replace("m-golf1-2","M. Golf 1-") 
-    name = name.replace("campeones","Liga Campeones-") 
-    name = name.replace("-", " ")  # Eliminar el guion "-"
-    name = " ".join(word.capitalize() for word in name.split())  # Convertir la primera letra de cada palabra en mayúscula
-    return name
-   
-   
+      
 def arrancar_acestream():
     
                    # if "linux" and "arm"  >> 'osmc','openelec','raspbian','raspios'
